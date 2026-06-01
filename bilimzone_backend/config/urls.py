4c3436b9aa321aac04122e6bd500fe2fd811be05
Aidanek Kadyrova <aidanekkadyrova@MacBook-Air-Aidanek.local>
2026-06-02 02:05:05 +0600

    Serve media files on Render

--- bilimzone_backend/config/urls.py
+++ bilimzone_backend/config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     path("api/", include("users.urls")),
     path("api/", include("publications.urls")),
     path("api/", include("categories.urls")),
@@ -16,5 +17,10 @@ urlpatterns = [
     path("api/", include("home.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
